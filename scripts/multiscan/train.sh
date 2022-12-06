fbfed4a727fe1045926903be2b0709bd1cb8d144
yma50 <yma50@cs-venus-01.cmpt.sfu.ca>
2022-12-05 17:33:03 -0800

    debug

--- scripts/multiscan/train.sh
+++ scripts/multiscan/train.sh
@@ -3,4 +3,4 @@ BASE_DIR='./'
 EXP_DIR="${BASE_DIR}/experiments/release/multiscan/exp0"
 cfg=./configs/multiscan/release.conf
 
-CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 python train.py --num_epochs=30 --DECAY_STEP_LIST 25 28 --cfg $cfg  --fullsize_eval=1 --use_test=0
+CUDA_VISIBLE_DEVICES=2 python train.py --num_epochs=15 --DECAY_STEP_LIST 12 14 --cfg $cfg  --fullsize_eval=1 --use_test=0
